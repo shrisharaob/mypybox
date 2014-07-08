47f7ce0f328f54a2546c6fdc289c824ed84d885f
shrisharaob <srao@pcneurophys76.biomedicale.univ-paris5.fr>
2014-07-08 15:47:01 +0200

    py startup file

---
+++ Startup.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pylab as plt
+import sys
+import os
+
+sys.path.append('/homecentral/srao/Documents/code/mypybox')
+#sys.path.append('/homecentral/srao/Documents/code/pylabbox')
